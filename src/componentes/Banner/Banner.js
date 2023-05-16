21428fcd1a189dae1173c59e31f66fe05b43e43f
Rafael Matthes <rafael.matthes@live.com>
2023-05-16 00:32:13 -0300

    add components

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,20 @@
+import './Banner.css'
+
+export const Banner = () => {
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="banner pg organo" />
+        </header>
+    )
+}
+
+// function Banner() {
+//     return (
+//     // JSX
+//         <header className="banner">
+//             <img src="/imagens/banner.png" alt="banner pg organo" />
+//         </header>
+//     );
+//   }
+
+// export default Banner
